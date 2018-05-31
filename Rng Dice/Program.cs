0796ad739b830673d199222283d1cad141d44c73
Mitchell Combs <mitch.combs.92@gmail.com>
2018-05-30 21:33:58 -0400

    Custom coded die roll generator, initial commit

--- Rng Dice/Program.cs
+++ Rng Dice/Program.cs
@@ -6,7 +6,51 @@ using System.Threading.Tasks;
 
 namespace Rng_Dice {
 	class Program {
+
+		int die = 0;
+		int score = 0;
+		Random rng = new Random();
+		int Games = 0;
+		int HighScore = 0;
+
+		int RollDie() {
+			return rng.Next(1, 7);
+		}
+
+
+		void ThrowDice() {
+			score = 0;
+			die = 0;
+
+			while(die != 1) {
+				score += die;
+				die = RollDie();
+			}
+		}
+
+
+
+
+		void Run() {
+			Games = 0;
+			while(score < 500) {
+				ThrowDice();
+
+				if (score > HighScore) {
+					HighScore = score;
+					Console.WriteLine("New highscore is {0}", HighScore);
+				}
+				
+
+				Games++;
+
+			}
+			
+
+		}
+
 		static void Main(string[] args) {
+			(new Program()).Run();
 		}
 	}
 }
